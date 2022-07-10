7d4a0f36ccd39235ab3c6df81378f53094f742bf
michaelchow1998 <chymichael.work@gmail.com>
2022-07-10 23:57:22 +0800

    Added: Order function - Check user exits and deducted inventory

--- src/main/java/com/michael/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/michael/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,19 @@ package com.michael.springbootmall.service.impl;
 
 import com.michael.springbootmall.dao.OrderDao;
 import com.michael.springbootmall.dao.ProductDao;
+import com.michael.springbootmall.dao.UserDao;
 import com.michael.springbootmall.dto.BuyItem;
 import com.michael.springbootmall.dto.CreateOrderRequest;
 import com.michael.springbootmall.model.Order;
 import com.michael.springbootmall.model.OrderItem;
 import com.michael.springbootmall.model.Product;
+import com.michael.springbootmall.model.User;
 import com.michael.springbootmall.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +28,9 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private ProductDao productDao;
 
+    @Autowired
+    private UserDao userDao;
+
     @Override
     public Order getOrderById(Integer orderId) {
         Order order = orderDao.getOrderById(orderId);
@@ -36,10 +43,28 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+
+        User user = userDao.getUserById(userId);
+
+        if(user == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
         for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            if(product == null){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }else if(product.getStock() < buyItem.getQuantity()){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            productDao.updateStock(product.getProductId(), product.getStock() - buyItem.getQuantity());
+
+
+
 
             int amount = buyItem.getQuantity() * product.getPrice();
             totalAmount = totalAmount + amount;
